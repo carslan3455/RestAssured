f96873ea7432d49a066fe5df5b1347611ade8279
carslan3455 <carslan3455@gmail.com>
2020-12-03 19:16:00 +0100

    add combiningTest

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,11 +1,88 @@
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
+
+    private ResponseSpecification responseSpecification;
+
+    @BeforeClass
+    public void setup() {
+
+        baseURI = "http://api.zippopotam.us";      // static REST Assured un kendi degiskeni
+        requestSpecification = new RequestSpecBuilder()
+                .log(LogDetail.URI)
+                .setAccept(ContentType.JSON)
+                .build();
+
+        responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .log(LogDetail.BODY)
+                .build();
+    }
+
+
+
+
+
+    @Test
+    public void bodyArraySizeTestBaseUri() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("/us/90210")       // todo http ile baslamiyorsa baseURI kullanacak
+                // BeforeClass da tanimladik baseURI yi
+
+                .then()
+                .log().body()
+                .body("places", hasSize(1))
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void bodyArraySizeTestrequestSpecification() {
+
+        given()
+                .spec(requestSpecification)     // todo BeforeClass icinde yazilanlari ayniyla cekip sorgu kullandik
+                .when()
+                .get("/us/90210")       // todo http ile baslamiyorsa baseURI kullanacak
+                // BeforeClass da tanimladik baseURI yi
+
+                .then()
+                .body("places", hasSize(1))
+                .spec(responseSpecification)    // todo BeforeClass icinde tanimlanan butun islemleri bu satir ile cagirmis oluyoruz
+
+        ;
+    }
+
+    @Test
+    public void bodyArraySizeTestResponseSpecification() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("/us/90210")       // todo http ile baslamiyorsa baseURI kullanacak
+                // BeforeClass da tanimladik baseURI yi
+
+                .then()
+                .body("places", hasSize(1))
+                .spec(responseSpecification)    // todo BeforeClass icinde tanimlanan butun islemleri bu satir ile cagirmis oluyoruz
+
+        ;
+    }
 
     @Test
     public void test() {
@@ -26,7 +103,7 @@ public class ZippoTest {
                 .then()
                 .log().all()  // sorguyu ekrana yazdiriyor
                 .statusCode(200)        // todo status islem kodunu assertion yapiyoruz. Dogr sonuc alinca farkedemeyebiliriz ama islem goruyor.
-                                        // Hatali kod yazsak ( mesela .statusCode(201) ) bu islem icin  hata mesaji veriyor
+        // Hatali kod yazsak ( mesela .statusCode(201) ) bu islem icin  hata mesaji veriyor
         ;
     }
 
@@ -40,13 +117,13 @@ public class ZippoTest {
                 .then()
                 .log().body()    // todo bu kisma all() gelirse tum bilgiler ekrana yazilmis oluyor. body() olunca sadece body kismi geliyor
                 .contentType(ContentType.JSON)   // gelen formatin JSON formatinda olup olmadigini kontrol ediyor
-                                                // normalde zaten JSON formatinda geliyor. Biz test etmis olduk.
-                                                 // contentType : Icerigin tipi
+        // normalde zaten JSON formatinda geliyor. Biz test etmis olduk.
+        // contentType : Icerigin tipi
         ;
     }
 
     @Test
-    public void logTest(){
+    public void logTest() {
 
         given()
                 .log().all()        // gondermeden onceki bilgiler
@@ -57,11 +134,11 @@ public class ZippoTest {
                 .then()
                 .log().body()       // gonderdikten sonra sadece body yazdirdik
 
-                ;
+        ;
     }
 
     @Test
-    public void bodyJsonPathTest(){  // Path Json daki bir ogeyi almak
+    public void bodyJsonPathTest() {  // Path Json daki bir ogeyi almak
 
         given()
                 .when()
@@ -69,16 +146,16 @@ public class ZippoTest {
 
                 .then()
                 .log().body()
-                .body("country",equalTo("United States"))  // body den kodu almadan kontrol etmeye yardimci oluyor
+                .body("country", equalTo("United States"))  // body den kodu almadan kontrol etmeye yardimci oluyor
                 .statusCode(200)
 
-                ;
+        ;
 
     }
 
 
     @Test
-    public void bodyJsonPathTest2(){  // Path Json daki bir ogeyi almak
+    public void bodyJsonPathTest2() {  // Path Json daki bir ogeyi almak
 
         given()
                 .when()
@@ -86,7 +163,7 @@ public class ZippoTest {
 
                 .then()
                 .log().body()
-                .body("places[0].state",equalTo("California"))  // body den kodu almadan kontrol etmeye yardimci oluyor
+                .body("places[0].state", equalTo("California"))  // body den kodu almadan kontrol etmeye yardimci oluyor
                 .statusCode(200)
 
         ;
@@ -94,7 +171,7 @@ public class ZippoTest {
     }
 
     @Test
-    public void bodyJsonPathTest3(){  // Path Json daki bir ogeyi almak
+    public void bodyJsonPathTest3() {  // Path Json daki bir ogeyi almak
 
         given()
                 .when()
@@ -102,7 +179,7 @@ public class ZippoTest {
 
                 .then()
                 .log().body()
-                .body("places[0].'place name'",equalTo("Beverly Hills"))  // body den kodu almadan kontrol etmeye yardimci oluyor
+                .body("places[0].'place name'", equalTo("Beverly Hills"))  // body den kodu almadan kontrol etmeye yardimci oluyor
                 // veri adinda arada bosluk varsa key basina '' konur. 'place name' seklinde
                 .statusCode(200)
 
@@ -111,7 +188,7 @@ public class ZippoTest {
     }
 
     @Test
-    public void bodyArraySizeTest(){
+    public void bodyArraySizeTest() {
 
         given()
                 .when()
@@ -119,15 +196,13 @@ public class ZippoTest {
 
                 .then()
                 .log().body()
-                .body("places",hasSize(1))  // places dizisinin uzunlugunu sorguladik
+                .body("places", hasSize(1))  // places dizisinin uzunlugunu sorguladik
                 .statusCode(200)
-
         ;
-
     }
 
     @Test
-    public void combiningTest(){
+    public void combiningTest() {
 
         given()
                 .when()
@@ -135,12 +210,68 @@ public class ZippoTest {
 
                 .then()
                 .log().body()
-                .body("places",hasSize(1))
-                .body("places[0].state",equalTo("California"))
-                .body("places[0].'place name'",equalTo("Beverly Hills"))
+                .body("places", hasSize(1))
+                .body("places[0].state", equalTo("California"))
+                .body("places[0].'place name'", equalTo("Beverly Hills"))
                 .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void pathParamTest() {
+
+        String country = "us";
+        String zipKod = "90210";
+
+        given()
+                .pathParam("country", country)
+                .pathParam("zipKod", zipKod)
+                .log().uri()   // calismadan once olusturulan URL verir
+                .when()
+                .get("http://api.zippopotam.us" + "/{country}/{zipKod}")
+
+                .then()
+                .body("places", hasSize(1))
+                .log().body()
 
         ;
-
     }
+    //  https://gorest.co.in/public-api/users?page=10
+
+    @Test
+    public void queryParamTest() {
+        int page = 10;
+
+        given()
+                .param("page", page)
+                .log().uri()
+
+                .when()
+                .get("https://gorest.co.in/public-api/users")
+
+                .then()
+                .log().body()
+                .body("meta.pagination.page", equalTo(page))     // verilen yerdeki bilgi esit mi?
+
+        ;
+    }
+
+    @Test
+    public void queryParamTestCoklu() {
+        for (int page = 1; page <= 10; page++) {      // page 1 olunca
+            given()
+                    .param("page", page)        // burasi da 1 uzantili olan sayfayi acacak
+                    .log().uri()
+
+                    .when()
+                    .get("https://gorest.co.in/public-api/users")
+
+                    .then()
+                    .log().body()
+                    .body("meta.pagination.page", equalTo(page))        // burda page karsiligi dongu devam ettikce 1,2,3....10 kadar devam
+            ;
+        }
+    }
+
+
 }
