06f1b563c67b32765ec25ccb0f52f9e73d9cd40c
carslan3455 <carslan3455@gmail.com>
2020-12-07 20:34:19 +0100

    task_6

--- src/test/java/Task/TaskSolution.java
+++ src/test/java/Task/TaskSolution.java
@@ -54,15 +54,16 @@ public class TaskSolution {
         ;
     }
 
-    /** Task 3
-     *  create a request to https://jsonplaceholder.typicode.com/todos/2
-     *  expect status 200
-     *  expect content type JSON
-     *  expect title in response body to be "quis ut nam facilis et officia qui"
+    /**
+     * Task 3
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect title in response body to be "quis ut nam facilis et officia qui"
      */
 
     @Test
-    public void task3(){
+    public void task3() {
 
         given()
                 .when()
@@ -72,8 +73,74 @@ public class TaskSolution {
                 .log().body()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("title",equalTo("quis ut nam facilis et officia qui"))
+                .body("title", equalTo("quis ut nam facilis et officia qui"))
 
+        ;
+    }
+
+
+    /**
+     * Task 4
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect completed in response body to be "false"
+     */
+
+    @Test
+    public void task4() {
+
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("completed", equalTo(false))  // burada "false" seklinde yazamayiz. String degil boolean cevap
+                //todo  .body("completed",equalTo(0>1))  (0>1) = false verdigi icin bu sekilde gelebilecek sartlar da olabilir
+        ;
+    }
+
+    /** Task 5
+     * create a request to https://jsonplaceholder.typicode.com/todos
+     * expect status 200
+     * expect content type JSON
+     * expect third item have:
+     *      title = "fugiat veniam minus"
+     *      userId = 1
+     */
+
+    @Test
+    public void task5() {
+
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("userId[2]",equalTo(1))
+                .body("title[2]",equalTo("fugiat veniam minus"))
                 ;
+    }
+
+    @Test
+    public void task6() {
+
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("userId[2]",equalTo(1))
+                .body("title[2]",equalTo("fugiat veniam minus"))
+        ;
     }
 }
